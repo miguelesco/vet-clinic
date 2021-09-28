0a7dd986ca1211a06d28243761907d5d9f8298cf
miguelesco <miuelangel1999@gmail.com>
2021-09-28 16:53:19 -0400

    update negative values

--- data.sql
+++ data.sql
@@ -6,9 +6,9 @@ VALUES
 	(2, 'Gabumon', 'Nov 15, 2018', 2, TRUE, 8),
 	(3, 'Pikachu', 'Jan 7, 2021', 1, FALSE, 15.04),
 	(4, 'Devimon', 'May 12, 2017', 5, TRUE, 11),
-	(5, 'Charmander', 'Feb 8, 2020', 0, FALSE, 11),
-	(6, 'Plantmon', 'Nov 15, 2022', 2, TRUE, 5.7),
-	(7, 'Squirtle', 'Apr 2, 1993', 3, FALSE, 12.13),
-	(8, 'Angemon', 'Jun 12, 2005', 1, TRUE, 45),
+	(5, 'Charmander', 'Feb 8, 2020', 0, FALSE, -11),
+	(6, 'Plantmon', 'Nov 15, 2022', 2, TRUE, -5.7),
+	(7, 'Squirtle', 'Apr 2, 1993', 3, FALSE, -12.13),
+	(8, 'Angemon', 'Jun 12, 2005', 1, TRUE, -45),
 	(9, 'Boarmon', 'Jun 7, 2005', 7, TRUE, 20.4),
 	(10, 'Blossom', 'Oct 13, 1998', 3, TRUE, 17);
