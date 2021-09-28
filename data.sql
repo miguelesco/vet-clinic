6176dd663f8ac3a52698076c3b1da94c68e74a2b
miguelesco <miuelangel1999@gmail.com>
2021-09-28 12:43:01 -0400

    update data and shma

--- data.sql
+++ data.sql
@@ -1,8 +1,14 @@
 /* Populate database with sample data. */
 
-INSERT INTO ANIMALS (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+INSERT INTO ANIMALS (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
 VALUES
 	(1, 'Agumon', 'Feb 3, 2020', 0, TRUE, 10.23),
 	(2, 'Gabumon', 'Nov 15, 2018', 2, TRUE, 8),
 	(3, 'Pikachu', 'Jan 7, 2021', 1, FALSE, 15.04),
-	(4, 'Devimon', 'May 12, 2017', 5, TRUE, 11)
+	(4, 'Devimon', 'May 12, 2017', 5, TRUE, 11),
+	(5, 'Charmander', 'Feb 8, 2020', 0, FALSE, 11, 'Animal'),
+	(6, 'Plantmon', 'Nov 15, 2022', 2, TRUE, 5.7, 'Animal'),
+	(7, 'Squirtle', 'Apr 2, 1993', 3, FALSE, 12.13, 'Animal'),
+	(8, 'Angemon', 'Jun 12, 2005', 1, TRUE, 45, 'Animal'),
+	(9, 'Boarmon', 'Jun 7, 2005', 7, TRUE, 20.4, 'Animal'),
+	(10, 'Blossom', 'Oct 13, 1998', 3, TRUE, 17, 'Animal');
