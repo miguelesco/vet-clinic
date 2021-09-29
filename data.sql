52e23d1b2fe995bbfe33a3deb8c9339647511b12
miguelesco <miuelangel1999@gmail.com>
2021-09-29 18:53:20 -0400

    update owners id, type and set data

--- data.sql
+++ data.sql
@@ -12,3 +12,34 @@ VALUES
 	(8, 'Angemon', 'Jun 12, 2005', 1, TRUE, -45),
 	(9, 'Boarmon', 'Jun 7, 2005', 7, TRUE, 20.4),
 	(10, 'Blossom', 'Oct 13, 1998', 3, TRUE, 17);
+
+/* Insert the following data into the owners table */
+INSERT INTO owners (full_name, age)
+VALUES 
+		('Sam Smith', 34),
+		('Jennifer Orwell', 19),
+		('Bob', 45),
+		('Melody Pond', 77),
+		('Dean Winchester', 14),
+		('Jodie Whittaker', 38);
+
+/* Insert the following data into the species table */
+INSERT INTO species (name)
+VALUES 
+		('Pokemon'),
+		('Digimon');
+
+/* Modify your inserted animals so it includes the species_id value: */
+UPDATE animals SET species_id = CASE WHEN name LIKE '%mon' THEN 2 ELSE 1 END;
+
+/* Modify your inserted animals to include owner information (owner_id) */
+BEGIN;
+UPDATE animals SET owner_id = CASE 
+	WHEN name IN ('Agumon') THEN 1
+	WHEN name IN ('Gabumon', 'Pikachu') THEN 2
+	WHEN name IN ('Devimon', 'Plantmon') THEN 3
+	WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN 4
+	WHEN name IN ('Angemon', 'Boarmon') THEN 5
+END;
+
+COMMIT;
